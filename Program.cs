140b5f8be16b8e6cd70f87a0e9a209fe272d83e6
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-04 11:07:09 +0200

    init

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ namespace _01.DataStructures
             numbers.Enqueue("two");
             numbers.Enqueue("three");
             numbers.Enqueue("four");
-           // numbers.Dequeue(); //removes the first element on the queue
+            // numbers.Dequeue(); //removes the first element on the queue
             //numbers.Peek(); --shows the next top elements withput removing it
 
             foreach (string item in numbers)  //enumerating teh queue ;iterating with foreaech  loop
@@ -50,14 +50,34 @@ namespace _01.DataStructures
             // statring at the middle
 
             string[] array2 = new string[numbers.Count * 2]; // create an array twice the size of numners Queue
-            numbers.CopyTo(array2, numbers.Count );  //We copied the array numbers to the array2
+            numbers.CopyTo(array2, numbers.Count);  //We copied the array numbers to the array2
 
-            
+
 
             foreach (var num in array2)
             {
                 Console.WriteLine(num);
             }
+            // Queues and stacks are usefuel when  You need a temporalily storage of information  !
+            //Objects stored in queue are inserted in one end and removed form the other End;
+            //Use Generic Queue <T> class , dont use Queue class- > use Generic Queue <T> class ! 
+            // non Generic collections shouldnt be used ! 
+            //use queue if You want to access the information in the same order that is stored in the collection
+            // Concept First In First Out !!
+
+            // Use stack if You need to access the informnaton in Reverse way .
+            //LIFO --concept-Last in First Out access of information !
+            // Method Enqueue () --adds element on the end of the Queue
+            // Dequeue()-removes the elemtne form the start of the queue-oldest element 
+            // Peek() -> returns the opldest element form the start of the Queue//
+            //-but does not remove it from the queue
+            // the capacity of the queue is the number of teh elements that it can hold !
+            // as the elements are addesd - the size of the queue is extended 
+            // TrimToSize();
+            // Queue  accepts null as a value, and can accept duoplicate elements !
+            // Queue.Count --gets teh numebr of the elements of teh Queue
+
+
         }
 
     }
